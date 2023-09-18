8b2bb0b8c5b7fe97ec3f2474a1d90fba5b4edb53
DavenK84 <145354427+DavenK84@users.noreply.github.com>
2023-09-18 14:39:06 +0100

    Add files via upload
    An iteration of part of the self driving algorithm for an autonomous race car. This program will use a StereoLabs ZED2 camera attached to an RC car to perform perception, object tracking, localisation and mapping. The car is driven around the track as part of a "formation lap" to collect information about it's surroundings and it's position in the surroundings over time.

---
+++ zed_DETECT14.py
@@ -0,0 +1,383 @@
+#python zed_DETECT11.py --weights runs\train\yolov7-cones\weights\best.pt --source 1
+
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+import csv
+import math
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages, letterbox
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from threading import Lock, Thread
+from time import sleep
+
+import ogl_viewer.viewer as gl
+#import ogl_viewer.tracking_viewer as track_gl
+import cv_viewer.tracking_viewer as cv_viewer
+import pyzed.sl as sl
+import numpy as np
+
+
+lock = Lock()
+run_signal = False
+exit_signal = False
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    #old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    zed = sl.Camera()
+    main()
+
+def img_preprocess(img, device, half, net_size):
+    net_image, ratio, pad = letterbox(img[:, :, :3], net_size, auto=False)
+    net_image = net_image.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    net_image = np.ascontiguousarray(net_image)
+
+    img = torch.from_numpy(net_image).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    return img, ratio, pad
+
+
+def xywh2abcd(xywh, im_shape):
+    output = np.zeros((4, 2))
+
+    # Center / Width / Height -> BBox corners coordinates
+    x_min = (xywh[0] - 0.5*xywh[2]) * im_shape[1]
+    x_max = (xywh[0] + 0.5*xywh[2]) * im_shape[1]
+    y_min = (xywh[1] - 0.5*xywh[3]) * im_shape[0]
+    y_max = (xywh[1] + 0.5*xywh[3]) * im_shape[0]
+
+    # A ------ B
+    # | Object |
+    # D ------ C
+
+    output[0][0] = x_min
+    output[0][1] = y_min
+
+    output[1][0] = x_max
+    output[1][1] = y_min
+
+    output[2][0] = x_min
+    output[2][1] = y_max
+
+    output[3][0] = x_max
+    output[3][1] = y_max
+    return output
+
+
+def detections_to_custom_box(detections, im, im0):
+    output = []
+    for i, det in enumerate(detections):
+        if len(det):
+            det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+
+            for *xyxy, conf, cls in reversed(det):
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+
+                # Creating ingestable objects for the ZED SDK
+                obj = sl.CustomBoxObjectData()
+                obj.bounding_box_2d = xywh2abcd(xywh, im0.shape)
+                obj.label = cls
+                obj.probability = conf
+                obj.is_grounded = False
+                output.append(obj)
+    return output
+
+
+def torch_thread(weights, img_size, conf_thres=0.2, iou_thres=0.45):
+    global image_net, exit_signal, run_signal, detections
+
+    print("Intializing Network...")
+
+    device = select_device()
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+    imgsz = img_size
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    if half:
+        model.half()  # to FP16
+    cudnn.benchmark = True
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+
+    while not exit_signal:
+        if run_signal:
+            lock.acquire()
+            img, ratio, pad = img_preprocess(image_net, device, half, imgsz)
+
+            pred = model(img)[0]
+            det = non_max_suppression(pred, conf_thres, iou_thres)
+
+            # ZED CustomBox format (with inverse letterboxing tf applied)
+            detections = detections_to_custom_box(det, img, image_net)
+            lock.release()
+            run_signal = False
+        key = cv2.waitKey(10)
+        if key == 27:
+            exit_signal = True
+        sleep(0.01)
+
+
+def main():
+    print("main")
+    global image_net, exit_signal, run_signal, detections
+
+    capture_thread = Thread(target=torch_thread, kwargs={'weights': opt.weights, 'img_size': opt.img_size, "conf_thres": opt.conf_thres})
+    capture_thread.start()
+
+    print("Initializing Camera...")
+
+    zed = sl.Camera()
+
+    input_type = sl.InputType()
+    #if opt.svo is not None:
+    #    input_type.set_from_svo_file(opt.svo)
+
+    # Create a InitParameters object and set configuration parameters
+    init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
+    init_params.coordinate_units = sl.UNIT.METER
+    init_params.depth_mode = sl.DEPTH_MODE.ULTRA  # QUALITY
+    init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
+    init_params.depth_maximum_distance = 50
+
+    runtime_params = sl.RuntimeParameters()
+    status = zed.open(init_params)
+
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit()
+
+    image_left_tmp = sl.Mat()
+
+    print("Initialized Camera")
+
+    positional_tracking_parameters = sl.PositionalTrackingParameters()
+    # If the camera is static, uncomment the following line to have better performances and boxes sticked to the ground.
+    # positional_tracking_parameters.set_as_static = True
+    zed.enable_positional_tracking(positional_tracking_parameters)
+
+    obj_param = sl.ObjectDetectionParameters()
+    obj_param.detection_model = sl.DETECTION_MODEL.CUSTOM_BOX_OBJECTS
+    obj_param.enable_tracking = True
+    zed.enable_object_detection(obj_param)
+
+    objects = sl.Objects()
+    obj_runtime_param = sl.ObjectDetectionRuntimeParameters()
+
+    # Display
+    camera_infos = zed.get_camera_information()
+    camera_res = camera_infos.camera_configuration.camera_resolution
+    # Create OpenGL viewer
+    viewer = gl.GLViewer()
+    #tracking_viewer = track_gl.GLViewer()
+    point_cloud_res = sl.Resolution(min(camera_res.width, 720), min(camera_res.height, 404))
+    point_cloud_render = sl.Mat()
+    viewer.init(camera_infos.camera_model, point_cloud_res, obj_param.enable_tracking)
+    #tracking_viewer.init(camera_infos.camera_model)
+    point_cloud = sl.Mat(point_cloud_res.width, point_cloud_res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
+    image_left = sl.Mat()
+    # Utilities for 2D display
+    display_resolution = sl.Resolution(min(camera_res.width, 1280), min(camera_res.height, 720))
+    image_scale = [display_resolution.width / camera_res.width, display_resolution.height / camera_res.height]
+    image_left_ocv = np.full((display_resolution.height, display_resolution.width, 4), [245, 239, 239, 255], np.uint8)
+
+    # Utilities for tracks view
+    camera_config = camera_infos.camera_configuration
+    tracks_resolution = sl.Resolution(400, display_resolution.height)
+    track_view_generator = cv_viewer.TrackingViewer(tracks_resolution, camera_config.camera_fps, init_params.depth_maximum_distance)
+    track_view_generator.set_camera_calibration(camera_config.calibration_parameters)
+    image_track_ocv = np.zeros((tracks_resolution.height, tracks_resolution.width, 4), np.uint8)
+    # Camera pose
+    cam_w_pose = sl.Pose()
+    
+    py_translation = sl.Translation()
+    pose_data = sl.Transform()
+
+    text_translation = ""
+    text_rotation = ""
+
+    csv_file = open('cone_depth2.csv', mode='w')
+    fieldnames = ['Cone ID', 'World Coordinates']
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+    while viewer.is_available() and not exit_signal:
+        if zed.grab(runtime_params) == sl.ERROR_CODE.SUCCESS:
+            # -- Get the image
+            lock.acquire()
+            zed.retrieve_image(image_left_tmp, sl.VIEW.LEFT)
+            image_net = image_left_tmp.get_data()
+            lock.release()
+            run_signal = True
+
+            # -- Detection running on the other thread
+            while run_signal:
+                sleep(0.001)
+
+            # Wait for detections
+            lock.acquire()
+            # -- Ingest detections
+            zed.ingest_custom_box_objects(detections)
+            lock.release()
+            zed.retrieve_objects(objects, obj_runtime_param)
+            
+            # -- Display
+            # Retrieve display data
+            zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA, sl.MEM.CPU, point_cloud_res)
+            point_cloud.copy_to(point_cloud_render)
+            zed.retrieve_image(image_left, sl.VIEW.LEFT, sl.MEM.CPU, display_resolution)
+            zed.get_position(cam_w_pose, sl.REFERENCE_FRAME.WORLD)
+
+            # 3D rendering
+            viewer.updateData(point_cloud_render, objects)
+            # 2D rendering
+            np.copyto(image_left_ocv, image_left.get_data())
+            cv_viewer.render_2D(image_left_ocv, image_scale, objects, obj_param.enable_tracking)
+            global_image = cv2.hconcat([image_left_ocv, image_track_ocv])
+            # Tracking view
+            track_view_generator.generate_view(objects, cam_w_pose, image_track_ocv, objects.is_tracked)
+
+            cv2.imshow("ZED | 2D View and Birds View", global_image)
+            key = cv2.waitKey(10)
+            
+            tracking_state = zed.get_position(cam_w_pose)
+            if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:
+                rotation = cam_w_pose.get_rotation_vector()
+                translation = cam_w_pose.get_translation(py_translation)
+                text_rotation = str((round(rotation[0], 2), round(rotation[1], 2), round(rotation[2], 2)))
+                text_translation = str((round(translation.get()[0], 2), round(translation.get()[1], 2), round(translation.get()[2], 2)))
+                translation_vector = np.array([round(translation.get()[0], 2), round(translation.get()[1], 2), round(translation.get()[2], 2)])
+                py_orientation = sl.Orientation()
+                q1 = round(cam_w_pose.get_orientation(py_orientation).get()[0], 3)
+                q2 = round(cam_w_pose.get_orientation(py_orientation).get()[1], 3)
+                q3 = round(cam_w_pose.get_orientation(py_orientation).get()[2], 3)
+                q0 = round(cam_w_pose.get_orientation(py_orientation).get()[3], 3)
+
+                # First row of the rotation matrix
+                r00 = 2 * (q0 * q0 + q1 * q1) - 1
+                r01 = 2 * (q1 * q2 - q0 * q3)
+                r02 = 2 * (q1 * q3 + q0 * q2)
+     
+                # Second row of the rotation matrix
+                r10 = 2 * (q1 * q2 + q0 * q3)
+                r11 = 2 * (q0 * q0 + q2 * q2) - 1
+                r12 = 2 * (q2 * q3 - q0 * q1)
+     
+                # Third row of the rotation matrix
+                r20 = 2 * (q1 * q3 - q0 * q2)
+                r21 = 2 * (q2 * q3 + q0 * q1)
+                r22 = 2 * (q0 * q0 + q3 * q3) - 1
+     
+                # 3x3 rotation matrix
+                rotation_matrix = np.array([[r00, r01, r02],
+                                       [r10, r11, r12],
+                                       [r20, r21, r22]])
+
+                #tracking_viewer.updateData(pose_data, text_translation, text_rotation, tracking_state)
+                for object in objects.object_list:
+                    #print("{} {}".format(object.id, object.position))
+                    object.position = np.dot(rotation_matrix, object.position.T)
+                    world_coordinates = translation_vector + object.position
+                    if abs(world_coordinates[2]) < 0.3:
+                        print("{} {}".format(object.id, world_coordinates))
+                        pass
+                    else:
+                        print("{} {}".format(object.id, world_coordinates))
+                        csv_writer.writerow({'Cone ID' : object.id, 'World Coordinates': world_coordinates})
+            
+            if key == 27:
+                exit_signal = True
+        else:
+            exit_signal = True
+
+    viewer.exit()
+#    tracking_viewer.exit()
+    exit_signal = True
+    zed.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7-tiny.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    # check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
